26eb60ef26460419e4288f9f7c2b823a8669b66d
Rodrigo Delgadillo Perez <rperez@rodrigos-mbp.home>
2020-02-16 20:05:12 -0600

    Add Unary API Server side

---
+++ greet/greet_server/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/grpc-go-tutorials/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstName := req.GetGreeting().GetFirsName()
+
+	result := "Hello " + firstName
+
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return res, nil
+
+}
+
+func main() {
+
+	fmt.Println("Hello")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatal("Failed to liste: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
